5b3a0599a1ce122d297880c6233eeaf1504b3481
JordanBandur <jordanbandur@hotmail.ca>
2024-05-02 08:24:56 -0700

    Refactor functions to interact with DB

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -8,9 +8,6 @@ const pool = new Pool({
   host: "localhost",
   database: "lightbnb",
 });
-pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {
-
-});
 
 /// Users
 
@@ -20,14 +17,19 @@ pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  const queryString = 'SELECT * FROM users WHERE email = $1';
+  return pool
+    .query(queryString, [email])
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return null;
+      } else {
+        return result.rows[0];
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /**
@@ -36,7 +38,19 @@ const getUserWithEmail = function(email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  const queryString = 'SELECT * FROM users WHERE id = $1';
+  return pool
+    .query(queryString, [id])
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return null;
+      } else {
+        return result.rows[0];
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /**
@@ -45,10 +59,15 @@ const getUserWithId = function(id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const queryString = 'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;';
+  return pool
+    .query(queryString, [user.name, user.email, user.password])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /// Reservations
