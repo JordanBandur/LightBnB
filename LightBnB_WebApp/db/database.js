be7c57fef3f0f96cb4bc287f9ee576856e8b8401
JordanBandur <jordanbandur@hotmail.ca>
2024-05-02 19:02:03 -0700

    Refactor addProperty to interact with DB

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -166,10 +166,31 @@ const getAllProperties = function(options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryString = `
+    INSERT INTO properties (
+      owner_id, title, description, thumbnail_photo_url, cover_photo_url, 
+      cost_per_night, street, city, province, post_code, country, 
+      parking_spaces, number_of_bathrooms, number_of_bedrooms
+    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *; `;
+
+  // Prepare an array of property values to be used in the query
+  const queryParams = [
+    property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url,
+    parseInt(property.cost_per_night * 100),
+    property.street, property.city, property.province, property.post_code, property.country,
+    property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms
+  ];
+
+  // Execute the query and return a promise containing the newly added property
+  return pool
+    .query(queryString, queryParams)
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.error('Error executing query', err.message);
+    });
 };
 
 module.exports = {
